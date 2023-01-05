350da002e52c20befb7c51ecbd996e68bd297010
Dklishch <klishch.dmytro@gmail.com>
2023-01-05 14:39:51 +0200

    Added maps texture loading to GameState

--- Source/MyProject2/MyGameState.h
+++ Source/MyProject2/MyGameState.h
@@ -19,50 +19,26 @@ class MYPROJECT2_API AMyGameState : public AGameStateBase
 {
 	GENERATED_BODY()
 public:
-	AMyGameState()
-	{
-		const ConstructorHelpers::FObjectFinder<UDataTable> ProvincesDescriptionFinder(TEXT("/Game/Sources/provinces_description"));
-		if (ProvincesDescriptionFinder.Succeeded())
-		{
-			ProvinceDescriptionDataTable = ProvincesDescriptionFinder.Object;
-		}
+	AMyGameState();
 
-		const ConstructorHelpers::FObjectFinder<UDataTable> CountriesDescriptionFinder(TEXT("/Game/Sources/countries_description"));
-		if (CountriesDescriptionFinder.Succeeded())
-		{
-			CountryDescriptionDataTable = CountriesDescriptionFinder.Object;
-		}
-		
-		const ConstructorHelpers::FObjectFinder<UDataTable> StateDescriptionFinder(TEXT("/Game/Sources/states_description"));
-		if (StateDescriptionFinder.Succeeded())
-		{
-			StateDescriptionDataTable = StateDescriptionFinder.Object;
-		}
-	}
+	FProvince* GetProvince(const FColor& ProvinceColor) const;
 
-	FProvince GetProvince(const FColor& ProvinceColor) const
-	{
-		if (ProvinceColor == FColor(0, 0, 0) || ProvinceColor == FColor(255, 255, 255)) return FProvince();
-		const FProvince* Province = reinterpret_cast<FProvince*>(ProvinceDescriptionDataTable->FindRowUnchecked(FName(ProvinceColor.ToHex())));
-		return Province ? *Province : FProvince();
-	}
+	FColor GetCountryColor(const FColor& ProvinceColor) const;
+
+	FState* GetState(const FString& StateId) const;
+
+	bool AreProvincesInTheSameState(FColor ProvinceAColor, FColor ProvinceBColor) const;
 	
-	FColor GetCountryColor(const FColor& ProvinceColor) const
-	{
-		if (ProvinceColor == FColor(0, 0, 0) || ProvinceColor == FColor(255, 255, 255)) return FColor(20, 20, 20);
-		const FProvince* Province = reinterpret_cast<FProvince*>(ProvinceDescriptionDataTable->FindRowUnchecked(FName(ProvinceColor.ToHex())));
-		if (!Province) return FColor(20, 20, 20);
+	bool AreProvincesNotInTheSameState(FColor ProvinceAColor, FColor ProvinceBColor) const;
 
-		const FCountry* Country = reinterpret_cast<FCountry*>(CountryDescriptionDataTable->FindRowUnchecked(FName(Province->CountryTag)));
-		return Country ? Country->Color : FColor(20, 20, 20);
-	}
+	UTexture2D* GetProvincesMapTexture() const;
 
-	FState GetState(const FString& StateId) const 
-	{
-		const FState* State = reinterpret_cast<FState*>(StateDescriptionDataTable->FindRowUnchecked(FName(StateId)));
-		return State ? *State : FState();
-	}
-	
+	UTexture2D* GetSelectionMapTexture() const;
+
+	UTexture2D* GetCountriesMapTexture() const;
+
+	UTexture2D* GetOutlinesMapTexture() const;
+
 	UPROPERTY()
 	UDataTable* CountryDescriptionDataTable;
 
@@ -71,4 +47,18 @@ public:
 
 	UPROPERTY()
 	UDataTable* StateDescriptionDataTable;
+
+private:
+	UPROPERTY()
+	UTexture2D* ProvincesMapTexture;
+
+	UPROPERTY()
+	UTexture2D* SelectionMapTexture;
+
+	UPROPERTY()
+	UTexture2D* CountriesMapTexture;
+
+	UPROPERTY()
+	UTexture2D* OutlinesMapTexture;
 };
+
