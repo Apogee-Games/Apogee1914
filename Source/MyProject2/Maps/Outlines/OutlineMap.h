b458d8592516bede31d79de0af8b70bc845b4112
DKlishch <klishch.dmytro@gmail.com>
2023-02-16 20:40:34 +0200

    Moved UOutlineMap and refactored it to use UProvincesMap

---
+++ Source/MyProject2/Maps/Outlines/OutlineMap.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "OutlineMap.generated.h"
+
+UCLASS()
+class UOutlineMap: public UWorldSubsystem
+{
+	GENERATED_BODY()
+public:
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	void CreateOutline();
+
+private:
+	UPROPERTY()
+	UTexture2D* OutlinesMapTexture;
+	
+	FVector2D SizeVector;
+
+	FColor OutlineColor = FColor(0, 0, 0);
+	
+	FColor BackgroundColor = FColor(255, 255, 255);
+};
