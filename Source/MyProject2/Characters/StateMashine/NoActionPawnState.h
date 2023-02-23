c7869cbb605c91c6b796bc82c68d0bd8ac6680e4
DKlishch <klishch.dmytro@gmail.com>
2023-02-23 11:28:53 +0200

    Added first state to state mashine

---
+++ Source/MyProject2/Characters/StateMashine/NoActionPawnState.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "PawnState.h"
+
+class FNoActionPawnState: public FPawnState
+{
+public:
+
+	static TSharedPtr<FNoActionPawnState> GetInstance();
+	
+	virtual TSharedPtr<FPawnState> LeftClick(AHumanPlayerPawn* Pawn) override;
+	
+	virtual TSharedPtr<FPawnState> RightClick(AHumanPlayerPawn* Pawn) override;
+	
+	virtual ~FNoActionPawnState() override;
+
+private:
+	FNoActionPawnState();
+	
+	inline static TSharedPtr<FNoActionPawnState> Instance;
+};
