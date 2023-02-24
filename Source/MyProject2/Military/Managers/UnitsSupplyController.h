4ac59374eb78e49d4af1386732911001b3abd9a9
DKlishch <klishch.dmytro@gmail.com>
2023-02-24 18:16:52 +0200

    Added system to control all countries suppliers

---
+++ Source/MyProject2/Military/Managers/UnitsSupplyController.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "MyProject2/Military/Instances/CountryUnitsSupplier.h"
+#include "MyProject2/Military/Interfaces/Observers/UnitCreationObserver.h"
+
+#include "UnitsSupplyController.generated.h"
+
+UCLASS()
+class UUnitsSupplyController: public UWorldSubsystem, public IUnitCreationObserver
+{
+	GENERATED_BODY()
+public:
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	
+	virtual void UnitIsCreated(UUnit* Unit) override;
+
+	void Supply();
+
+private:
+	UPROPERTY()
+	TMap<UCountry*, UCountryUnitsSupplier*> CountrySupplier;
+};
