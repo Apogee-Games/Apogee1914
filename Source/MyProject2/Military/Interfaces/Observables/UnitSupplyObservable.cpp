1c3e457994179b88ac1c1b7b56eeb8ac9eac5376
DKlishch <klishch.dmytro@gmail.com>
2023-02-24 19:41:44 +0200

    Added unit supply observer and observable interfaces

---
+++ Source/MyProject2/Military/Interfaces/Observables/UnitSupplyObservable.cpp
@@ -0,0 +1,23 @@
+#include "UnitSupplyObservable.h"
+
+void IUnitSupplyObservable::NotifyUnitSupply(UUnit* Unit)
+{
+	for (auto& Observer: Observers)
+	{
+		Observer->UnitIsSupplied(Unit);
+	}
+}
+
+bool IUnitSupplyObservable::AddUnitSupplyObserver(IUnitSupplyObserver* Observer)
+{
+	if (Observers.Contains(Observer)) return false;
+	Observers.Add(Observer);
+	return true;
+}
+
+bool IUnitSupplyObservable::RemoveUnitSupplyObserver(IUnitSupplyObserver* Observer)
+{
+	if (!Observers.Contains(Observer)) return false;
+	Observers.Remove(Observer);
+	return true;
+}
