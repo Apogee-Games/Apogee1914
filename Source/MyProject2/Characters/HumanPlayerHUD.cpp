62d28bf7acfdef40d4e8f9a17be25703e7defafd
DKlishch <klishch.dmytro@gmail.com>
2023-03-01 19:28:16 +0200

    Added class for HUD

---
+++ Source/MyProject2/Characters/HumanPlayerHUD.cpp
@@ -0,0 +1,1 @@
+#include "HumanPlayerHUD.h"
---
+++ Source/MyProject2/Characters/HumanPlayerHUD.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "GameFramework/HUD.h"
+#include "HumanPlayerHUD.generated.h"
+
+UCLASS()
+class AHumanPlayerHUD: public AHUD
+{
+	GENERATED_BODY()
+public:
+	
+};
