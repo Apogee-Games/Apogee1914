e1bc19f9cfcdfb87bd2c420488c269beb2725852
DKlishch <klishch.dmytro@gmail.com>
2023-03-02 16:18:33 +0200

    Created unit description and good requiremnt carriers

---
+++ Source/MyProject2/Military/Instances/UnitDescriptionCarrier.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "MyProject2/Military/Descriptions/UnitDescription.h"
+#include "UnitDescriptionCarrier.generated.h"
+
+UCLASS()
+class UUnitDescriptionCarrier : public UObject
+{
+	GENERATED_BODY()
+public:
+	void Init(const FUnitDescription* ProvidedUnitDescription);
+
+	const FUnitDescription* GetUnitDescription() const;
+	
+private:
+	const FUnitDescription* UnitDescription;
+};
