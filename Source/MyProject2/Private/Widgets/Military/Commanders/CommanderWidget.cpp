b7619b2021333940a6aabb15803601cb7e81f07b
KlishchD <klishch.dmytro@gmail.com>
2023-03-14 23:30:22 +0200

    Added commanders widgets

---
+++ Source/MyProject2/Private/Widgets/Military/Commanders/CommanderWidget.cpp
@@ -0,0 +1,37 @@
+﻿
+#include "Widgets/Military/Commanders/CommanderWidget.h"
+
+#include "Characters/HumanPlayerPawn.h"
+#include "Characters/StateMachine/MapBrowsingPawnState.h"
+#include "Military/Managers/CommandersManager.h"
+
+void UCommanderWidget::SetCommander(UObject* ProvidedCommander)
+{
+	Commander = Cast<UPerson>(ProvidedCommander);
+}
+
+void UCommanderWidget::RefreshData()
+{
+	CommanderImage->SetBrushResourceObject(Commander->GetImage());
+
+	AgeTextBlock->SetText(FText::FromString(FString::FromInt(Commander->GetAge())));
+	
+	NameTextBlock->SetText(FText::FromName(Commander->GetPersonName()));
+}
+
+void UCommanderWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	Button->OnClicked.AddDynamic(this, &UCommanderWidget::OnButtonClick);
+}
+
+void UCommanderWidget::OnButtonClick()
+{
+	AHumanPlayerPawn* Pawn = GetOwningPlayerPawn<AHumanPlayerPawn>();
+	Pawn->SetPawnState(FMapBrowsingPawnState::GetInstance());
+
+	if (ICommandable* Commandable = Pawn->GetSelectedCommandable())
+	{
+		GetWorld()->GetSubsystem<UCommandersManager>()->MakeCommander(Commander, Commandable);
+	}
+}
---
+++ Source/MyProject2/Public/Widgets/Military/Commanders/CommanderWidget.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "People/Instances/Person.h"
+#include "CommanderWidget.generated.h"
+
+UCLASS()
+class UCommanderWidget: public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UImage* CommanderImage;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UTextBlock* AgeTextBlock;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UTextBlock* NameTextBlock;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UButton* Button; 
+
+	virtual void NativeConstruct() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void SetCommander(UObject* ProvidedCommander);
+
+	UFUNCTION(BlueprintCallable)
+	void RefreshData();
+
+private:
+	UPROPERTY()
+	UPerson* Commander;
+
+	UFUNCTION()
+	void OnButtonClick();
+};
