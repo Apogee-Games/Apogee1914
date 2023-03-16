0b634badc8757f6d602f7888f99d4385b0022346
KlishchD <klishch.dmytro@gmail.com>
2023-03-16 20:07:53 +0200

    Added Scenario

---
+++ Source/MyProject2/Private/Scenario.cpp
@@ -0,0 +1,1 @@
+﻿#include "Scenario.h"
---
+++ Source/MyProject2/Public/Scenario.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include "Engine/DataTable.h"
+#include "Scenario.generated.h"
+
+UCLASS()
+class UScenario: public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* CountryDescriptionDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* ProvinceDescriptionDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* TerrainDescriptionDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* ResourcesDescriptionDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* StateDescriptionDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* GoodDescriptionDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* StrataDescriptionDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UDataTable* EventsDataTable;
+
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* SelectionMapTexture;
+
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* ProvincesMapTexture;
+
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* OutlinesMapTexture;
+
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* FlagsMapTexture;
+
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* CountriesMapTexture;
+};
