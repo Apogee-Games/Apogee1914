08a857abab7583537b8f58888806555bbcd4130e
KlishchD <klishch.dmytro@gmail.com>
2023-03-17 13:17:50 +0200

    Added scenario description widget

---
+++ Source/MyProject2/Private/Widgets/CountrySelection/ScenarioDescriptionWidget.cpp
@@ -0,0 +1,25 @@
+﻿#include "Widgets/CountrySelection/ScenarioDescriptionWidget.h"
+
+#include "Characters/HUDs/CountrySelectionHUD.h"
+#include "Characters/Pawns/CountrySelectionPawn.h"
+
+void UScenarioDescriptionWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	Button->OnClicked.AddDynamic(this, &UScenarioDescriptionWidget::OnButtonClick);
+}
+
+void UScenarioDescriptionWidget::SetScenario(UObject* ProvidedScenario)
+{
+	Scenario = Cast<UScenario>(ProvidedScenario);
+}
+
+void UScenarioDescriptionWidget::RefreshData()
+{
+	ScenarioNameTextBlock->SetText(FText::FromName(Scenario->ScenarioName));
+}
+
+void UScenarioDescriptionWidget::OnButtonClick()
+{
+	GetOwningPlayer()->GetHUD<ACountrySelectionHUD>()->GetScenarioSelectionWidget()->SetScenario(Scenario);
+}
