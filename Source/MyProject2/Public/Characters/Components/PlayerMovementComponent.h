eb52fbf791dbd4f760adc0d399739fc89e53cc22
KlishchD <klishch.dmytro@gmail.com>
2023-03-17 11:32:43 +0200

    Added player movement component

---
+++ Source/MyProject2/Public/Characters/Components/PlayerMovementComponent.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "MapActor.h"
+#include "PlayerMovementComponent.generated.h"
+
+UCLASS()
+class UPlayerMovementComponent: public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	void Init(AMapActor* ProvidedMapActor);
+	
+	void MoveUp(float Value);
+
+	void MoveRight(float Value);
+
+	void Move(float DeltaTime);
+
+	void Scroll(float Value);
+private:
+	FVector MovementDirection = FVector(0, 0, 0);
+
+	FRotator RotationDirection = FRotator(0, 0, 0);
+
+	FVector SpeedVector = FVector(10, 10, 10);
+	
+	UPROPERTY()
+	AMapActor* MapActor;
+};
