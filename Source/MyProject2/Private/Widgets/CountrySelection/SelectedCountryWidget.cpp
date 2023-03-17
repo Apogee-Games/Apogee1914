705e3df58de0cf1714f1ef1bfe38be1fcf4580a0
KlishchD <klishch.dmytro@gmail.com>
2023-03-17 13:19:40 +0200

    Added selected country widget

---
+++ Source/MyProject2/Private/Widgets/CountrySelection/SelectedCountryWidget.cpp
@@ -0,0 +1,8 @@
+﻿#include "Widgets/CountrySelection/SelectedCountryWidget.h"
+
+void USelectedCountryWidget::SetCountry(UCountry* Country)
+{
+	CountryFlagImage->SetBrushResourceObject(Country->GetFlag());
+	CountryNameTextBlock->SetText(FText::FromName(Country->GetName()));
+	//RulingFractionNameTextBlock->SetText(FText::FromName(Country->))
+}
---
+++ Source/MyProject2/Public/Widgets/CountrySelection/SelectedCountryWidget.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Administration/Instances/Country.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "SelectedCountryWidget.generated.h"
+
+UCLASS()
+class USelectedCountryWidget: public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UImage* CountryFlagImage;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UTextBlock* CountryNameTextBlock;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UTextBlock* RulingFractionNameTextBlock;
+
+	void SetCountry(UCountry* Country);
+};
