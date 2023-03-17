04da4b7062a103c95d71fa38f888b5e72d918575
KlishchD <klishch.dmytro@gmail.com>
2023-03-17 13:25:44 +0200

    Added country selection pawn state

---
+++ Source/MyProject2/Public/Characters/StateMachine/CountrySelectionPawnState.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "PawnState.h"
+
+class FCountrySelectionPawnState: public FPawnState
+{
+public:
+	static TSharedPtr<FPawnState> GetInstance();
+
+	virtual TSharedPtr<FPawnState> LeftClick(APawn* ProvidedPawn) override;
+	
+	virtual TSharedPtr<FPawnState> RightClick(APawn* ProvidedPawn) override;
+
+	virtual bool MustWidgetBeVisible(UUserWidget* Widget) override;
+	
+	virtual bool CanWidgetBeVisible(UUserWidget* Widget) override;
+	
+private:
+	FCountrySelectionPawnState();
+	
+	inline static TSharedPtr<FCountrySelectionPawnState> Instance;
+};
