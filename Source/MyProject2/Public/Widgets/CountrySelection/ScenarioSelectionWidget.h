a90df46c955cd770c0a5b502890c4661492c72f1
KlishchD <klishch.dmytro@gmail.com>
2023-03-17 13:18:51 +0200

    Added scenario selection widget

---
+++ Source/MyProject2/Public/Widgets/CountrySelection/ScenarioSelectionWidget.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Scenario.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Components/ListView.h"
+#include "Components/TextBlock.h"
+#include "ScenarioSelectionWidget.generated.h"
+
+UCLASS()
+class UScenarioSelectionWidget: public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UListView* ScenariosListView;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UTextBlock* ActiveScenarioNameTextBlock;
+	
+	virtual void NativeConstruct() override;
+
+	void SetScenario(UScenario* Scenario);
+};
