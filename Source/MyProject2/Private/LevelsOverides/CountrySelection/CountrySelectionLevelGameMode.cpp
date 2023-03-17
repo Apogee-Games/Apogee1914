5a33f314dc02b7dd59a977fa6cac2278effadc9f
KlishchD <klishch.dmytro@gmail.com>
2023-03-17 13:30:32 +0200

    Set default pawn hud

--- Source/MyProject2/Private/LevelsOverides/CountrySelection/CountrySelectionLevelGameMode.cpp
+++ Source/MyProject2/Private/LevelsOverides/CountrySelection/CountrySelectionLevelGameMode.cpp
@@ -1,10 +1,12 @@
 ﻿#include "LevelsOverides/CountrySelection/CountrySelectionLevelGameMode.h"
 
+#include "Characters/HUDs/CountrySelectionHUD.h"
 #include "Characters/Pawns/CountrySelectionPawn.h"
 
 ACountrySelectionLevelGameMode::ACountrySelectionLevelGameMode()
 {
 	DefaultPawnClass = ACountrySelectionPawn::StaticClass();
+	HUDClass = ACountrySelectionHUD::StaticClass();
 }
 
 void ACountrySelectionLevelGameMode::PostLogin(APlayerController* NewPlayer)
