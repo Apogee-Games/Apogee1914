23f50c866a01b8f001c3886e63511877e1816b44
KlishchD <klishch.dmytro@gmail.com>
2023-03-20 20:23:21 +0200

    Added wars list widgets

---
+++ Source/MyProject2/Private/Widgets/Diplomacy/Wars/List/WarsListWidget.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "Widgets/Diplomacy/Wars/List/WarsListWidget.h"
+
+#include "Characters/Pawns/HumanPlayerPawn.h"
+#include "Diplomacy/Managers/RelationshipsManager.h"
+
+void UWarsListWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	GetGameInstance()->GetSubsystem<URelationshipsManager>()->AddWarDeclarationObserver(this);
+}
+
+void UWarsListWidget::WarWasDeclared(UWar* War)
+{
+	UCountry* OwnersCountry = GetOwningPlayerPawn<AHumanPlayerPawn>()->GetRuledCountry();
+	if (War->CountryParticipates(OwnersCountry))
+	{
+		WarsListView->AddItem(War);
+	}
+}
+
+void UWarsListWidget::RemoveWar(UWar* War)
+{
+	WarsListView->RemoveItem(War);
+}
+
+void UWarsListWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+	GetGameInstance()->GetSubsystem<URelationshipsManager>()->RemoveWarDeclarationObserver(this);
+}
---
+++ Source/MyProject2/Public/Widgets/Diplomacy/Wars/List/WarsListWidget.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Blueprint/UserWidget.h"
+#include "Components/ListView.h"
+#include "Diplomacy/Instances/War.h"
+#include "Diplomacy/Interfaces/Observers/WarDeclarationObserver.h"
+#include "WarsListWidget.generated.h"
+
+UCLASS()
+class UWarsListWidget: public UUserWidget, public IWarDeclarationObserver
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UListView* WarsListView;
+
+	virtual void NativeConstruct() override;
+	
+	virtual void WarWasDeclared(UWar* War) override;
+
+	void RemoveWar(UWar* War);
+
+	virtual void NativeDestruct() override;
+};
