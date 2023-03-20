8df536781fd4e932c29b2fc1d833e00acadeaf66
KlishchD <klishch.dmytro@gmail.com>
2023-03-20 20:28:17 +0200

    Added diplomatic pawn states

---
+++ Source/MyProject2/Private/Characters/StateMachine/CountryDiplomacyPawnState.cpp
@@ -0,0 +1,61 @@
+﻿
+#include "Characters/StateMachine/CountryDiplomacyPawnState.h"
+
+#include "Characters/HUDs/HumanPlayerHUD.h"
+#include "Characters/Pawns/HumanPlayerPawn.h"
+#include "Characters/StateMachine/MapBrowsingPawnState.h"
+#include "Maps/Diplomacy/CountryRelationMap.h"
+#include "Maps/Selection/SelectionMap.h"
+#include "Widgets/Diplomacy/CountryDiplomacyWidget.h"
+
+TSharedPtr<FPawnState> FCountryDiplomacyPawnState::GetInstance()
+{
+	if (!Instance)
+	{
+		Instance = TSharedPtr<FCountryDiplomacyPawnState>(new FCountryDiplomacyPawnState);
+	}
+	return Instance;
+}
+
+TSharedPtr<FPawnState> FCountryDiplomacyPawnState::LeftClick(APawn* ProvidedPawn)
+{
+	return FMapBrowsingPawnState::GetInstance()->LeftClick(ProvidedPawn);
+}
+
+TSharedPtr<FPawnState> FCountryDiplomacyPawnState::RightClick(APawn* ProvidedPawn)
+{
+	USelectionMap* SelectionMap = ProvidedPawn->GetGameInstance()->GetSubsystem<USelectionMap>();
+
+	AHumanPlayerPawn* Pawn = Cast<AHumanPlayerPawn>(ProvidedPawn);
+
+	FVector2d Point = Pawn->MapActor->GetMapPosition(Pawn);
+	
+	UProvince* Province = SelectionMap->GetProvince(Point);
+
+	if (!Province) return Instance;
+
+	AHumanPlayerHUD* HUD = Pawn->GetController<APlayerController>()->GetHUD<AHumanPlayerHUD>();
+	
+	UCountryDiplomacyWidget* Widget = HUD->GetCountryDiplomacyWidget();
+	
+	Widget->SetCountry(Province->GetCountryController());
+
+	Pawn->GetGameInstance()->GetSubsystem<UCountryRelationMap>()->UpdateMap(); // TODO: Add logic for one country
+
+	return Instance;
+}
+
+bool FCountryDiplomacyPawnState::MustWidgetBeVisible(UUserWidget* Widget)
+{
+	return dynamic_cast<UCountryDiplomacyWidget*>(Widget) != nullptr;
+}
+
+bool FCountryDiplomacyPawnState::CanWidgetBeVisible(UUserWidget* Widget)
+{
+	return false;
+}
+
+FCountryDiplomacyPawnState::FCountryDiplomacyPawnState()
+{
+}
+
