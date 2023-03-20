b89c18e3f32ae4d53e6df4d5bfe10423e1ba6ce1
KlishchD <klishch.dmytro@gmail.com>
2023-03-20 20:24:05 +0200

    Added wars descriptions widget

---
+++ Source/MyProject2/Public/Widgets/Diplomacy/Wars/Description/WarParticipantWidget.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Administration/Instances/Country.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "WarParticipantWidget.generated.h"
+
+UCLASS()
+class UWarParticipantWidget: public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UImage* FlagImage;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	UTextBlock* NameTextBlock;
+
+	UFUNCTION(BlueprintCallable)
+	void SetCountry(UObject* ProvidedCountry);
+
+	UFUNCTION(BlueprintCallable)
+	void RefreshData();
+private:
+	UPROPERTY()
+	UCountry* Country;
+};
