ebf9b89e8fb1e1730ff0e0a2ee277758b5165590
KlishchD <klishch.dmytro@gmail.com>
2023-03-20 20:26:57 +0200

    Added diplomatic map

---
+++ Source/MyProject2/Private/Maps/Diplomacy/CountryRelationMap.cpp
@@ -0,0 +1,54 @@
+﻿
+#include "Maps/Diplomacy/CountryRelationMap.h"
+
+#include "Administration/Managers/CountriesManager.h"
+#include "Administration/Managers/ProvinceManager.h"
+#include "Maps/Precalculations/ProvincesMap.h"
+#include "Utils/TextureUtils.h"
+
+
+void UCountryRelationMap::SetScenario(UScenario* Scenario)
+{
+	Clear();
+	Init(Scenario);
+}
+
+void UCountryRelationMap::UpdateMap()
+{
+	// TODO: Check getters :)
+
+	FColor* Colors = FTextureUtils::GetPixels(CountryRelationMap, LOCK_READ_WRITE);
+
+	const TArray<UProvince*>& Provinces = GetGameInstance()->GetSubsystem<UProvinceManager>()->GetAllProvinces(); 
+	UProvincesMap* ProvincesMap = GetGameInstance()->GetSubsystem<UProvincesMap>();
+	URelationshipsManager* RelationshipsManager = GetGameInstance()->GetSubsystem<URelationshipsManager>();
+
+	UCountry* Germany = GetGameInstance()->GetSubsystem<UCountriesManager>()->GetCountry("GER");
+	
+	for (const auto& Province: Provinces)
+	{
+		// TODO: Add for each country :)
+		
+		FColor Color = ColorsMapping[RelationshipsManager->GetRelation(Germany, Province->GetCountryController())];
+		for (const int32 Position: ProvincesMap->GetProvincePositions(Province->GetId()))
+		{
+			Colors[Position] = Color;
+		}
+	}
+
+	FTextureUtils::UnlockPixels(CountryRelationMap);
+	
+	CountryRelationMap->UpdateResource();
+}
+
+void UCountryRelationMap::Clear()
+{
+	CountryRelationMap = nullptr;
+}
+
+void UCountryRelationMap::Init(UScenario* Scenario)
+{
+	CountryRelationMap = Scenario->CountryRelationMapTexture;
+	SizeVector = FTextureUtils::GetTextureSizeVector(CountryRelationMap);
+	UpdateMap();
+}
---
+++ Source/MyProject2/Public/Maps/Diplomacy/CountryRelationMap.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Scenario.h"
+#include "Diplomacy/Managers/RelationshipsManager.h"
+#include "CountryRelationMap.generated.h"
+
+UCLASS()
+class UCountryRelationMap: public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	void SetScenario(UScenario* Scenario);
+
+	void UpdateMap();
+
+private:
+	UPROPERTY()
+	UTexture2D* CountryRelationMap;
+
+	FVector2d SizeVector;
+
+	TMap<ERelationType, FColor> ColorsMapping = {
+		{ERelationType::Neutral, FColor(30, 30, 30)},
+		{ERelationType::NonAggressionPact, FColor(0, 30, 30)},
+		{ERelationType::War, FColor(150, 10, 10)},
+		{ERelationType::DefencivePact, FColor(30, 150, 30)},
+		{ERelationType::Allied, FColor(30, 100, 150)}
+	};
+
+	void Clear();
+
+	void Init(UScenario* Scenario);
+};
