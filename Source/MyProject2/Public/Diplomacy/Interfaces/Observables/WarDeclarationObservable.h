9fc7ee95553bbc02b40bce0b496032f7cdfa4157
KlishchD <klishch.dmytro@gmail.com>
2023-03-20 20:22:05 +0200

    Added diplomacy interfaces

---
+++ Source/MyProject2/Public/Diplomacy/Interfaces/Observables/WarDeclarationObservable.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Diplomacy/Instances/War.h"
+#include "Diplomacy/Interfaces/Observers/WarDeclarationObserver.h"
+
+class IWarDeclarationObservable
+{
+public:
+	void NotifyWarDeclaration(UWar* War);
+	
+	bool AddWarDeclarationObserver(IWarDeclarationObserver* Observer);
+	
+	bool RemoveWarDeclarationObserver(IWarDeclarationObserver* Observer);
+
+	void ClearObservers();
+private:
+	TArray<IWarDeclarationObserver*> Observers;
+};
