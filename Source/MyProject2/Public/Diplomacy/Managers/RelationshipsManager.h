cd202a2dfbfc2d6e4c01e237ff05352005b2fede
KlishchD <klishch.dmytro@gmail.com>
2023-03-21 18:49:55 +0200

    Added alliance creation logic to relationship maanager

--- Source/MyProject2/Public/Diplomacy/Managers/RelationshipsManager.h
+++ Source/MyProject2/Public/Diplomacy/Managers/RelationshipsManager.h
@@ -2,7 +2,9 @@
 #include "Scenario.h"
 #include "Administration/Instances/Country.h"
 #include "Components/ListView.h"
+#include "Diplomacy/Interfaces/Observables/AllianceCreationObservable.h"
 #include "Diplomacy/Interfaces/Observables/WarDeclarationObservable.h"
+#include "Widgets/Diplomacy/CountryDiplomacyWidget.h"
 
 #include "RelationshipsManager.generated.h"
 
@@ -15,8 +17,8 @@ enum ERelationType {
 	Allied = 16
 };
 
-UCLASS()
-class URelationshipsManager: public UGameInstanceSubsystem, public IWarDeclarationObservable
+UCLASS(Abstract, Blueprintable)
+class URelationshipsManager: public UGameInstanceSubsystem, public IWarDeclarationObservable, public IAllianceCreationObservable
 {
 	GENERATED_BODY()
 public:
@@ -30,7 +32,7 @@ public:
 
 	bool CanCreateAlliance(UCountry* CountryA, UCountry* CountryB);
 
-	void SetWarRelation(UCountry* CountryA, UCountry* CountryB);
+	void SetRelation(UCountry* CountryA, UCountry* CountryB, ERelationType Relation);
 	
 	void DeclareWar(UCountry* CountryA, UCountry* CountryB);
 
@@ -38,11 +40,25 @@ public:
 
 	void CreateDefencivePact(UCountry* CountryA, UCountry* CountryB);
 
+	void CreateAlliance(UCountry* Country, const FText& AllianceName);
+
+	void CreateAlliance(UCountry* Country, const FText& AllianceName, const TArray<UCountry*>& InvitedCountries);
+	
 	ERelationType GetRelation(UCountry* CountryA, UCountry* CountryB);
 
 	const TMap<UCountry*, ERelationType>& GetRelations(UCountry* Country);
+
+	const TArray<UWar*>& GetCountryWars(UCountry* Country);
+
+	bool IsCountryInWar(UCountry* Country);
+
+	bool CanCountryJoinWar(UCountry* CountryA, UCountry* CountryB);
+	
+	bool IsTherePossibleWarToJoin(UCountry* CountryToJoin, UCountry* CountryToJoinTo);
 private:
 	TMap<UCountry*, TMap<UCountry*, ERelationType>> Relations;
+
+	TMap<UCountry*, TArray<UWar*>> CountriesWars;
 
 	inline static int32 CanDeclareWarList = 0b00001;
 
@@ -51,6 +67,12 @@ private:
 	inline static int32 CanCreateDefencivePactList = 0b00011;
 
 	inline static int32 CanCreateAllianceList = 0b01011;
+
+	inline static int32 MustComeToDefense = 0b11000;
+
+	inline static int32 MustHelpInAggressiveWar = 0b10000;
+
+	inline static int32 CanJoinWar = 0b10000;
 
 	void Clear();
 
