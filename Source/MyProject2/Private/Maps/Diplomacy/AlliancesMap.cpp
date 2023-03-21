d21fe8df3ff4991fa95afecda9e52307c06c599c
KlishchD <klishch.dmytro@gmail.com>
2023-03-21 18:37:14 +0200

    Added allainces map class

---
+++ Source/MyProject2/Private/Maps/Diplomacy/AlliancesMap.cpp
@@ -0,0 +1,64 @@
+﻿#include "Maps/Diplomacy/AlliancesMap.h"
+
+#include "MyGameInstance.h"
+#include "Administration/Instances/Province.h"
+#include "Administration/Managers/ProvinceManager.h"
+#include "Maps/Precalculations/ProvincesMap.h"
+#include "Utils/TextureUtils.h"
+
+void UAlliancesMap::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	Collection.InitializeDependency(Cast<UMyGameInstance>(GetGameInstance())->RelationshipsManagerClass);
+	GetGameInstance()->GetSubsystem<URelationshipsManager>()->AddAllianceCreationObserver(this);
+}
+
+void UAlliancesMap::SetScenario(UScenario* Scenario)
+{
+	Clear();
+	Init(Scenario);
+}
+
+void UAlliancesMap::UpdateMap()
+{
+	TArray<UProvince*> Provinces = GetGameInstance()->GetSubsystem<UProvinceManager>()->GetAllProvinces();
+	UProvincesMap* ProvincesMap = GetGameInstance()->GetSubsystem<UProvincesMap>();
+
+	FColor* Colors = FTextureUtils::GetPixels(AlliancesMapTexture, LOCK_READ_WRITE);
+	
+	for (const auto& Province: Provinces)
+	{
+		FColor Color;
+
+		if (Province->GetCountryController()->GetAlliance())
+		{
+			Color = Province->GetCountryController()->GetAlliance()->GetColor();
+		} else
+		{
+			Color = NonAlliedCountryColor;
+		}
+		
+		for (const auto& Position: ProvincesMap->GetProvincePositions(Province->GetId()))
+		{
+			Colors[Position] = Color;
+		}
+	}
+
+	FTextureUtils::UnlockPixels(AlliancesMapTexture);
+	AlliancesMapTexture->UpdateResource();
+}
+
+void UAlliancesMap::AllianceWasCreated(UAlliance* Alliance)
+{
+	UpdateMap();
+}
+
+void UAlliancesMap::Clear()
+{
+	AlliancesMapTexture = nullptr;
+}
+
+void UAlliancesMap::Init(UScenario* Scenario)
+{
+	AlliancesMapTexture = Scenario->AlliancesMapTexture;
+}
