905b3b183c92e6ce89f841e64e2533701c9ea3c7
KlishchD <klishch.dmytro@gmail.com>
2023-03-21 12:20:01 +0200

    Added alliance creation pawn state

---
+++ Source/MyProject2/Private/Characters/StateMachine/AllianceCreationPawnState.cpp
@@ -0,0 +1,38 @@
+﻿#include "Characters/StateMachine/AllianceCreationPawnState.h"
+
+#include "Characters/StateMachine/MapBrowsingPawnState.h"
+#include "Characters/StateMachine/PawnState.h"
+#include "Widgets/Diplomacy/Alliance/Creation/AllianceCreationWidget.h"
+
+TSharedPtr<FPawnState> FAllianceCreationPawnState::GetInstance()
+{
+	if (!Instance)
+	{
+		Instance = TSharedPtr<FAllianceCreationPawnState>(new FAllianceCreationPawnState);
+	}
+	return Instance;
+}
+
+TSharedPtr<FPawnState> FAllianceCreationPawnState::LeftClick(APawn* ProvidedPawn)
+{
+	return FMapBrowsingPawnState::GetInstance()->LeftClick(ProvidedPawn);
+}
+
+TSharedPtr<FPawnState> FAllianceCreationPawnState::RightClick(APawn* ProvidedPawn)
+{
+	return FMapBrowsingPawnState::GetInstance()->RightClick(ProvidedPawn);
+}
+
+bool FAllianceCreationPawnState::MustWidgetBeVisible(UUserWidget* Widget)
+{
+	return dynamic_cast<UAllianceCreationWidget*>(Widget) != nullptr;
+}
+
+bool FAllianceCreationPawnState::CanWidgetBeVisible(UUserWidget* Widget)
+{
+	return false;
+}
+
+FAllianceCreationPawnState::FAllianceCreationPawnState()
+{
+}
---
+++ Source/MyProject2/Public/Characters/StateMachine/AllianceCreationPawnState.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "PawnState.h"
+
+class FAllianceCreationPawnState: public FPawnState
+{
+public:
+	static TSharedPtr<FPawnState> GetInstance();
+
+	virtual TSharedPtr<FPawnState> LeftClick(APawn* ProvidedPawn) override;
+	
+	virtual TSharedPtr<FPawnState> RightClick(APawn* ProvidedPawn) override;
+	
+	virtual bool MustWidgetBeVisible(UUserWidget* Widget) override;
+	
+	virtual bool CanWidgetBeVisible(UUserWidget* Widget) override;
+	
+private:
+	FAllianceCreationPawnState();
+	
+	inline static TSharedPtr<FAllianceCreationPawnState> Instance;
+};
