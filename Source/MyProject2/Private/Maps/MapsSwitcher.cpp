bd1f4d65b49e46ad27a6ae4ab74a213da43eb51a
KlishchD <klishch.dmytro@gmail.com>
2023-03-21 10:43:01 +0200

    Addded maps switcher

---
+++ Source/MyProject2/Private/Maps/MapsSwitcher.cpp
@@ -0,0 +1,49 @@
+﻿#include "Maps/MapsSwitcher.h"
+
+void UMapsSwitcher::SetScenario(UScenario* ProvidedScenario)
+{
+	Scenario = ProvidedScenario;
+}
+
+void UMapsSwitcher::SelectFlagsMap(AHumanPlayerPawn* Pawn)
+{
+	UMaterialInstanceDynamic* MapsMaterial = Pawn->MapActor->MapsMaterialInstanceDynamic;
+	MapsMaterial->SetTextureParameterValue(TEXT("Front"), Scenario->FlagsMapTexture);
+}
+
+void UMapsSwitcher::SelectCountriesMap(AHumanPlayerPawn* Pawn)
+{
+	UMaterialInstanceDynamic* MapsMaterial = Pawn->MapActor->MapsMaterialInstanceDynamic;
+	MapsMaterial->SetTextureParameterValue(TEXT("Front"), Scenario->CountriesMapTexture);
+}
+
+void UMapsSwitcher::SelectRelationMap(AHumanPlayerPawn* Pawn)
+{
+	UMaterialInstanceDynamic* MapsMaterial = Pawn->MapActor->MapsMaterialInstanceDynamic;
+	MapsMaterial->SetTextureParameterValue(TEXT("Front"), Scenario->CountryRelationMapTexture);
+}
+
+void UMapsSwitcher::SetApplyProvinceOutlineMap(AHumanPlayerPawn* Pawn, bool Flag)
+{
+	UMaterialInstanceDynamic* MapsMaterial = Pawn->MapActor->MapsMaterialInstanceDynamic;
+	if (Flag)
+	{
+		MapsMaterial->SetTextureParameterValue(TEXT("Outlines"), Scenario->OutlinesMapTexture);
+	} else
+	{
+		MapsMaterial->SetTextureParameterValue(TEXT("Outlines"), BlankMapTexture);
+	}
+}
+
+void UMapsSwitcher::SetApplySelectionMap(AHumanPlayerPawn* Pawn, bool Flag)
+{
+	UMaterialInstanceDynamic* MapsMaterial = Pawn->MapActor->MapsMaterialInstanceDynamic;
+	if (Flag)
+	{
+		MapsMaterial->SetTextureParameterValue(TEXT("Selection"), Scenario->SelectionMapTexture);
+	} else
+	{
+		MapsMaterial->SetTextureParameterValue(TEXT("Selection"), BlankMapTexture);
+	}
+}
+
---
+++ Source/MyProject2/Public/Maps/MapsSwitcher.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "Scenario.h"
+#include "Characters/Pawns/HumanPlayerPawn.h"
+
+#include "MapsSwitcher.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class UMapsSwitcher: public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* BlankMapTexture;
+
+	UPROPERTY(BlueprintReadOnly)
+	UScenario* Scenario;
+	
+	void SetScenario(UScenario* ProvidedScenario);
+
+	void SelectFlagsMap(AHumanPlayerPawn* Pawn);
+
+	void SelectCountriesMap(AHumanPlayerPawn* Pawn);
+
+	void SelectRelationMap(AHumanPlayerPawn* Pawn);
+
+	void SetApplyProvinceOutlineMap(AHumanPlayerPawn* Pawn, bool Flag);
+
+	void SetApplySelectionMap(AHumanPlayerPawn* Pawn, bool Flag);
+};
