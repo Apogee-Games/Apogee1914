16af0f40fee442f90eb1522ba203410098d1b637
KlishchD <klishch.dmytro@gmail.com>
2023-03-21 18:35:49 +0200

    Added alliance class

---
+++ Source/MyProject2/Private/Diplomacy/Instances/Alliance.cpp
@@ -0,0 +1,46 @@
+﻿#include "Diplomacy/Instances/Alliance.h"
+
+#include "Diplomacy/Managers/RelationshipsManager.h"
+
+void UAlliance::Init(UCountry* ProvidedAllianceLeader, const FText& ProvidedAllianceName)
+{
+	ProvidedAllianceLeader->SetAlliance(this);
+	AllianceLeader = ProvidedAllianceLeader;
+	Members.Add(AllianceLeader);
+	AllianceName = ProvidedAllianceName;
+}
+
+void UAlliance::AddMember(UCountry* Country)
+{
+	URelationshipsManager* RelationshipsManager = Cast<URelationshipsManager>(GetOuter());
+	for (const auto& Member: Members)
+	{
+		RelationshipsManager->SetRelation(Country, Member, Allied);
+	}
+	
+	Members.Add(Country);
+	Country->SetAlliance(this);
+
+}
+
+void UAlliance::RemoveMember(UCountry* Country)
+{
+	Members.Remove(Country);
+	Country->SetAlliance(nullptr);
+
+	URelationshipsManager* RelationshipsManager = Cast<URelationshipsManager>(GetOuter());
+	for (const auto& Member: Members)
+	{
+		RelationshipsManager->SetRelation(Country, Member, Neutral);
+	}
+}
+
+const FText& UAlliance::GetName() const
+{
+	return AllianceName;
+}
+
+FColor UAlliance::GetColor() const
+{
+	return AllianceLeader->GetColor();
+}
---
+++ Source/MyProject2/Public/Diplomacy/Instances/Alliance.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Administration/Instances/Country.h"
+
+#include "Alliance.generated.h"
+
+UCLASS()
+class UAlliance: public UObject
+{
+	GENERATED_BODY()
+public:
+	void Init(UCountry* ProvidedAllianceLeader, const FText& ProvidedAllianceName);
+
+	void AddMember(UCountry* Country);
+
+	void RemoveMember(UCountry* Country);
+
+	const FText& GetName() const;
+
+	FColor GetColor() const;
+private:
+	FText AllianceName;
+
+	UPROPERTY()
+	UCountry* AllianceLeader;
+
+	UPROPERTY()
+	TArray<UCountry*> Members;
+};
