6aecfbc2f9387caa941e3d3d91a7ff9b802d9491
KlishchD <klishch.dmytro@gmail.com>
2023-03-23 14:22:55 +0200

    Added fractions

---
+++ Source/MyProject2/Private/Administration/Instances/Fraction.cpp
@@ -0,0 +1,25 @@
+﻿#include "Administration/Instances/Fraction.h"
+
+#include "Administration/Managers/IdeologyManager.h"
+
+void UFraction::Init(const FFractionDescription& ProvidedFractionDescription)
+{
+	Ideology = GetWorld()->GetGameInstance()->GetSubsystem<UIdeologyManager>()->GetIdeology(ProvidedFractionDescription.IdeologyTag);
+	FractionName = ProvidedFractionDescription.FractionName;
+	MembersNumber = ProvidedFractionDescription.MembersNumber;
+}
+
+UIdeology* UFraction::GetIdeology() const
+{
+	return Ideology;
+}
+
+const FText& UFraction::GetFractionName() const
+{
+	return FractionName;
+}
+
+int32 UFraction::GetMembersNumber() const
+{
+	return MembersNumber;
+}
---
+++ Source/MyProject2/Public/Administration/Instances/Fraction.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Ideology.h"
+#include "Administration/Descriptions/FractionDescription.h"
+#include "Fraction.generated.h"
+
+UCLASS()
+class UFraction: public UObject
+{
+	GENERATED_BODY()
+public:
+	void Init(const FFractionDescription& ProvidedFractionDescription);
+
+	UIdeology* GetIdeology() const;
+
+	const FText& GetFractionName() const;
+
+	int32 GetMembersNumber() const;
+private:
+	UPROPERTY()
+	UIdeology* Ideology;
+
+	FText FractionName;
+
+	int32 MembersNumber;
+};
