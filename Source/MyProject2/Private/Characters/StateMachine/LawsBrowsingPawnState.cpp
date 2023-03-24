e9ceab4dc47814221fe344c8437854e560a9ef0f
KlishchD <klishch.dmytro@gmail.com>
2023-03-24 15:11:38 +0200

    Added laws browsing pawn state

---
+++ Source/MyProject2/Private/Characters/StateMachine/LawsBrowsingPawnState.cpp
@@ -0,0 +1,39 @@
+﻿#include "Characters/StateMachine/LawsBrowsingPawnState.h"
+
+#include "Characters/StateMachine/MapBrowsingPawnState.h"
+#include "Widgets/Administration/Laws/LawsGroupWidget.h"
+#include "Widgets/Administration/Laws/LawsWidget.h"
+
+TSharedPtr<FPawnState> FLawsBrowsingPawnState::GetInstance()
+{
+	if (!Instance)
+	{
+		Instance = TSharedPtr<FLawsBrowsingPawnState>(new FLawsBrowsingPawnState);
+	}
+	return Instance;
+}
+
+TSharedPtr<FPawnState> FLawsBrowsingPawnState::LeftClick(APawn* ProvidedPawn)
+{
+	return Instance;
+}
+
+TSharedPtr<FPawnState> FLawsBrowsingPawnState::RightClick(APawn* ProvidedPawn)
+{
+	return Instance;
+}
+
+bool FLawsBrowsingPawnState::MustWidgetBeVisible(UUserWidget* Widget)
+{
+	return dynamic_cast<ULawsWidget*>(Widget) != nullptr;
+}
+
+bool FLawsBrowsingPawnState::CanWidgetBeVisible(UUserWidget* Widget)
+{
+	return false;
+}
+
+FLawsBrowsingPawnState::FLawsBrowsingPawnState()
+{
+	
+}
---
+++ Source/MyProject2/Public/Characters/StateMachine/LawsBrowsingPawnState.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "PawnState.h"
+
+class FLawsBrowsingPawnState: public FPawnState 
+{
+public:
+	static TSharedPtr<FPawnState> GetInstance();
+	
+	virtual TSharedPtr<FPawnState> LeftClick(APawn* ProvidedPawn) override;
+	
+	virtual TSharedPtr<FPawnState> RightClick(APawn* ProvidedPawn) override;
+
+	virtual bool MustWidgetBeVisible(UUserWidget* Widget) override;
+	
+	virtual bool CanWidgetBeVisible(UUserWidget* Widget) override;
+private:
+	FLawsBrowsingPawnState();
+	
+	inline static TSharedPtr<FLawsBrowsingPawnState> Instance;
+};
