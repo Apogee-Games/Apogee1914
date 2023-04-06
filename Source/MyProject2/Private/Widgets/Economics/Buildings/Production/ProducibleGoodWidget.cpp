77938f8424dceaa0b1bf4ae9029279a579b808ee
KlishchD <klishch.dmytro@gmail.com>
2023-04-06 13:37:11 +0300

    Added production selection widgets

---
+++ Source/MyProject2/Private/Widgets/Economics/Buildings/Production/ProducibleGoodWidget.cpp
@@ -0,0 +1,27 @@
+﻿#include "Widgets/Economics/Buildings/Production/ProducibleGoodWidget.h"
+#include "Characters/Pawns/HumanPlayerPawn.h"
+#include "Characters/StateMachine/ProductionBrowsingPawnState.h"
+
+void UProducibleGoodWidget::SetGood(UObject* ProvidedGood)
+{
+	Good = Cast<UCookedGoodDescription>(ProvidedGood);
+}
+
+void UProducibleGoodWidget::RefreshData()
+{
+	GoodImage->SetBrushResourceObject(Good->Image);
+	GoodNameTextBlock->SetText(Good->NameText);
+}
+
+void UProducibleGoodWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	Button->OnClicked.AddDynamic(this, &UProducibleGoodWidget::OnButtonClick);
+}
+
+void UProducibleGoodWidget::OnButtonClick()
+{
+	AHumanPlayerPawn* Pawn = GetOwningPlayerPawn<AHumanPlayerPawn>();
+	Pawn->GetSelectedFactory()->SetGoodToProduce(Good);
+	Pawn->SetPawnState(FProductionBrowsingPawnState::GetInstance());
+}
