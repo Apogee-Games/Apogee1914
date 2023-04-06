4bc9a9a28736a351d21202b5d14dbbf5d567b4b9
KlishchD <klishch.dmytro@gmail.com>
2023-04-06 13:31:03 +0300

    Added goods groups and subgroups

---
+++ Source/MyProject2/Public/Economics/Description/Goods/GoodsGroup.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "GoodsSubgroup.h"
+#include "GoodsGroup.generated.h"
+
+UCLASS()
+class UGoodsGroup : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly)
+	FText GroupName;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TArray<UGoodsSubgroup*> Subgroups;
+};
---
+++ Source/MyProject2/Public/Economics/Description/Goods/GoodsSubgroup.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "GoodDescription.h"
+#include "GoodsSubgroup.generated.h"
+
+UCLASS()
+class UGoodsSubgroup : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly)
+	FText SubgroupName;
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<UGoodDescription*> Goods;
+};
