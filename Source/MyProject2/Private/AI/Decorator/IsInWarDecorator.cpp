4ee5d0199d0eb0052fab8f6743fb0a0c6f03deb8
KlishchD <klishch.dmytro@gmail.com>
2023-04-08 20:35:03 +0300

    Added test AI logic

---
+++ Source/MyProject2/Private/AI/Decorator/IsInWarDecorator.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Decorator/IsInWarDecorator.h"
+
+#include "Characters/AIPlayerPawn.h"
+
+bool UIsInWarDecorator::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	AController* Controller = Cast<AController>(OwnerComp.GetOwner());
+
+	if (!Controller)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	AAIPlayerPawn* Pawn = Cast<AAIPlayerPawn>(Controller->GetPawn());
+
+	if (!Pawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	return Pawn->GetRuledCountry()->IsInWar();
+}
---
+++ Source/MyProject2/Private/AI/Tasks/AdvanceTroopsTask.cpp
@@ -0,0 +1,78 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Tasks/AdvanceTroopsTask.h"
+
+#include "MyGameInstance.h"
+
+EBTNodeResult::Type UAdvanceTroopsTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	if (Super::ExecuteTask(OwnerComp, NodeMemory) != EBTNodeResult::Succeeded)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	UMyGameInstance* GameInstance = GetWorld()->GetGameInstance<UMyGameInstance>();
+	UProvincesMap* ProvincesMap = GameInstance->GetSubsystem<UProvincesMap>();
+	UProvinceManager* ProvinceManager = GameInstance->GetSubsystem<UProvinceManager>();
+	UUnitsMover* UnitsMover = GameInstance->GetSubsystem<UUnitsMover>();
+	
+	const TMap<FColor, TSet<FColor>>& Neigbours = ProvincesMap->GetNeighbours();
+
+	TArray<UUnit*> MovableUnits;
+	TArray<UProvince*> Provinces;
+	
+	for (const auto& Province: Country->GetProvinces())
+	{
+		TArray<UUnit*> Units = Province->GetUnits();
+
+		if (Units.IsEmpty()) continue;
+		
+		bool HasEnemyNeighbour = false;
+		
+		for (const auto& NeighbourColor: Neigbours[Province->GetId()])
+		{
+			UProvince* NeighbourProvince = ProvinceManager->GetProvince(NeighbourColor);
+			if (NeighbourProvince && NeighbourProvince->GetCountryController()->IsInWarWith(Country))
+			{
+				HasEnemyNeighbour = true;
+				Provinces.Add(NeighbourProvince);
+			} 
+		}
+
+		if (HasEnemyNeighbour)
+		{
+			for (const auto& Unit: Units)
+			{
+				MovableUnits.Add(Unit);
+			}
+		}
+	}
+
+	if (MovableUnits.IsEmpty() || Provinces.IsEmpty()) return EBTNodeResult::Succeeded;
+	
+	if (MovableUnits.Num() > Provinces.Num())
+	{
+		int Size = MovableUnits.Num() / Provinces.Num();
+		for (int i = 0; i < Provinces.Num(); ++i)
+		{
+			for (int j = 0; j < Size && i * Size + j < MovableUnits.Num(); ++j)
+			{
+				UnitsMover->MoveUnit(MovableUnits[i * Size + j], Provinces[i]);
+			}
+		}
+		for (int i = Provinces.Num() * Size; i < MovableUnits.Num(); ++i)
+		{
+			UnitsMover->MoveUnit(MovableUnits[i], Provinces[0]);
+		}
+	} else
+	{
+		for (int i = 0; i < MovableUnits.Num(); ++i)
+		{
+			UnitsMover->MoveUnit(MovableUnits[i], Provinces[i]);
+		}
+	}
+	
+
+	return EBTNodeResult::Succeeded;
+}
