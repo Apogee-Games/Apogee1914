b35319efc697b74c0abd3ec0129ef8f48ee6db6b
AlexVolkov0404 <90132916+AlexVolkov0404@users.noreply.github.com>
2023-04-11 22:10:21 +0300

    reformat code

--- Source/MyProject2/Private/Characters/Components/SphereMapActor.cpp
+++ Source/MyProject2/Private/Characters/Components/SphereMapActor.cpp
@@ -1,42 +1,39 @@
-   #include "Characters/Components/SphereMapActor.h"
+#include "Characters/Components/SphereMapActor.h"
 
 #include "VectorTypes.h"
 
 
-  FVector3d ASphereMapActor::GetNewPosition(FVector3d Position, FVector3d Direction, float Lenght)
-{ 	  	
-  	 Direction.Normalize();
-  	 return Position + Direction * Lenght;
+FVector3d ASphereMapActor::GetNewPosition(FVector3d Position, FVector3d Direction, float Lenght)
+{
+	Direction.Normalize();
+	return Position + Direction * Lenght;
 }
 
 FVector2d ASphereMapActor::GetMapPosition(FVector3d WorldPosition)
 {
-  	FVector Vector= WorldPosition-SphereCenter;
-  	if(Radius==-1) Radius=Vector.Size();
-  	float theta = atan2( WorldPosition.X - 159,WorldPosition.Y - 95) / (2 * PI) + 0.5;
-  	float phi = acos((WorldPosition.Z - 81) / Radius) / PI;  
-  	return FVector2d( theta,phi);
+	FVector Vector = WorldPosition - SphereCenter;
+	if (Radius == -1) Radius = Vector.Size();
+	float theta = atan2(WorldPosition.X - 159, WorldPosition.Y - 95) / (2 * PI) + 0.5;
+	float phi = acos((WorldPosition.Z - 81) / Radius) / PI;
+	return FVector2d(theta, phi);
 }
 
 
 FVector3d ASphereMapActor::GetWorldPosition(FVector2d MapPosition)
 {
-  	float theta = (MapPosition.X-0.5) * PI * 2;
-  	float phi = (MapPosition.Y ) * PI;
-  	float x = 95 +  Radius* sin(phi) * sin(theta);
-  	float y = 159 + Radius * cos(theta) * sin(phi);
-  	float z = 81 + Radius * cos(phi);  	
-  	return FVector3d(x, y, z);
-  	
+	float theta = (MapPosition.X - 0.5) * PI * 2;
+	float phi = (MapPosition.Y) * PI;
+	float x = 95 + Radius * sin(phi) * sin(theta);
+	float y = 159 + Radius * cos(theta) * sin(phi);
+	float z = 81 + Radius * cos(phi);
+	return FVector3d(x, y, z);
 }
 
 FQuat ASphereMapActor::GetNewRotation(FVector3d Position, FVector3d NewPosition)
 {
-  	Position -= SphereCenter;
-  	NewPosition -= SphereCenter;  	
-  	FVector3d RotationAxis = FVector3d::CrossProduct(Position, NewPosition).GetSafeNormal();
-  	float RotationAngle = FMath::Acos(FVector3d::DotProduct(Position.GetSafeNormal(), NewPosition.GetSafeNormal()));
-  	return FQuat(RotationAxis, RotationAngle);
+	Position -= SphereCenter;
+	NewPosition -= SphereCenter;
+	FVector3d RotationAxis = FVector3d::CrossProduct(Position, NewPosition).GetSafeNormal();
+	float RotationAngle = FMath::Acos(FVector3d::DotProduct(Position.GetSafeNormal(), NewPosition.GetSafeNormal()));
+	return FQuat(RotationAxis, RotationAngle);
 }
-
-
--- Source/MyProject2/Public/Characters/Components/SphereMapActor.h
+++ Source/MyProject2/Public/Characters/Components/SphereMapActor.h
@@ -3,17 +3,19 @@
 #include "SphereMapActor.generated.h"
 
 UCLASS()
-class  ASphereMapActor : public AMapActor
+class ASphereMapActor : public AMapActor
 {
 	GENERATED_BODY()
 
 public:
-	float Radius=-1;
-	UPROPERTY(EditDefaultsOnly);
-	FVector3d SphereCenter=FVector3d (159, 95, 81);	
-	
+	float Radius = -1;
+
+	UPROPERTY(EditDefaultsOnly)
+	;
+	FVector3d SphereCenter = FVector3d(159, 95, 81);
+
 	virtual FVector3d GetNewPosition(FVector3d Position, FVector3d Direction, float Lenght) override;
-	
+
 	virtual FVector2d GetMapPosition(FVector3d WorldPosition) override;
 
 	virtual FVector3d GetWorldPosition(FVector2d MapPosition) override;
