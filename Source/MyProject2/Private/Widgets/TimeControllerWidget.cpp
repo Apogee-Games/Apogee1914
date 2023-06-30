d534de445539b34e30b9402a82d392a205728a39
Dmytro Klishch <dklishch@n-ix.com>
2023-06-30 15:38:44 +0300

    Added tooltip class and added it to TimeControllerWidget

--- Source/MyProject2/Private/Widgets/TimeControllerWidget.cpp
+++ Source/MyProject2/Private/Widgets/TimeControllerWidget.cpp
@@ -8,6 +8,9 @@ void UTimeControllerWidget::NativeConstruct()
 	SpeedUpButton->OnClicked.AddDynamic(this, &UTimeControllerWidget::SpeedUpButtonOnClick);
 	SlowDownButton->OnClicked.AddDynamic(this, &UTimeControllerWidget::SlowDownButtonOnClick);
 	PauseButton->OnClicked.AddDynamic(this, &UTimeControllerWidget::PauseButtonOnClick);
+
+	TimeToolTipWidget = CreateWidget<UToolTipWidget>(this, ToolTipWidgetClass);
+	SetToolTip(TimeToolTipWidget);
 	
 	SetTime(GetGameInstance()->GetSubsystem<UInGameTime>()->GetStringTime());
 }
@@ -15,6 +18,7 @@ void UTimeControllerWidget::NativeConstruct()
 void UTimeControllerWidget::SetTime(const FText& Time)
 {
 	TimeTextBlock->SetText(Time);
+	TimeToolTipWidget->SetData(Time);
 }
 
 void UTimeControllerWidget::SetSpeedPercentage(float NewSpeedPercentage)
